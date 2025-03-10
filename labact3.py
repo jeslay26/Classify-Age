04f5e8e316dd15b76e2465286538d8dec8409283
jeslay26 <jeslienadinezamora@gmail.com>
2025-03-10 19:29:20 +0800

    Add files via upload

---
+++ labact3.py
@@ -0,0 +1,14 @@
+def classify_age(age):
+    if age < 0:
+        print("Invalid age.")
+    elif age <= 12:
+        print("You are a Child.")
+    elif age <= 19:
+        print("You are a Teen.")
+    elif age <= 64:
+        print("You are an Adult.")
+    else:
+        print("You are a Senior.")
+
+age = int(input("How old are you? "))
+classify_age(age)
